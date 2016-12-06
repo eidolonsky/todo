d9f04ca63ea5849721988ab40ad038c333314277
TianJiang <riersy2324@gmail.com>
2016-12-06 18:46:35 +0800

    remove scaffold style;add mark as complete function

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -8,10 +9,14 @@ class TodoItemsController < ApplicationController
   end
 
   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id])
     @todo_item.destroy
     redirect_to @todo_list
   end  
+
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Completed"
+  end
 
   private
 
@@ -19,6 +24,10 @@ class TodoItemsController < ApplicationController
     @todo_list = TodoList.find(params[:todo_list_id])
   end
   
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end  
+
   def todo_item_params
     params[:todo_item].permit(:content)
   end
