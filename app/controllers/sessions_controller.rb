076d79d3580ce6216c6ba240722825620cef7f81
TianJiang <riersy2324@gmail.com>
2016-12-07 17:30:50 +0800

    redef create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,20 @@
 class SessionsController < ApplicationController
+
   def create
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    session[:id] = @user.id
+    begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.name}!"
+    end
     redirect_to root_path
   end
+  
   def destroy
     if current_user
-      session.delete(:id)
+      session.delete(:user_id)
+      flash[:success] = 'See you!'
     end
-    redirect_to root_path    
-  end
+    redirect_to root_path
+  end 
+   
 end
